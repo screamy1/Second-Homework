b4a00a1f028a6810955c90564a0101dff052f012
Denis <szyabkin@inbox.ru>
2019-10-13 00:47:44 +0300

    final

--- app/src/main/java/com/example/secondhomework/bonch/dev/school/FirstActivity.kt
+++ app/src/main/java/com/example/secondhomework/bonch/dev/school/FirstActivity.kt
@@ -1,12 +1,12 @@
 package com.example.secondhomework.bonch.dev.school
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import com.example.secondhomework.R
 import com.example.secondhomework.bonch.dev.school.Models.Counter
-import kotlinx.android.synthetic.main.activity_first.*
 
 class FirstActivity : AppCompatActivity() {
 
@@ -15,18 +15,22 @@ class FirstActivity : AppCompatActivity() {
     private lateinit var textField: TextView
     private lateinit var nextActivityButton: Button
     private lateinit var counter: Counter
+    private var indic: Boolean = true
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_first)
         initializeViews()
-        setListener()
+        setListeners()
 
         counter = if (savedInstanceState == null) {
             Counter()
         } else {
             Counter(savedInstanceState.getInt("TAP_AMOUNTS"))
         }
+
+        counterButton.text = "Button was tapped ${counter.currentCount} times"
+
     }
 
     private fun initializeViews() {
@@ -36,10 +40,28 @@ class FirstActivity : AppCompatActivity() {
         counterButton = findViewById(R.id.counter_button)
     }
 
-    private fun setListener() {
+    private fun setListeners() {
         counterButton.setOnClickListener {
             counter.increment()
             counterButton.text = "Button was tapped ${counter.currentCount} times"
         }
+        nextActivityButton.setOnClickListener {
+            val intent = Intent(FirstActivity@this, SecondActivity::class.java)
+            intent.putExtra("INDICATOR", indic)
+            intent.putExtra("TAP_AMOUNTS", counter.currentCount)
+            intent.putExtra("INPUT_TEXT", textField.text.toString())
+            startActivity(intent)
+        }
+        indicatorButton.setOnClickListener {
+            indicatorButton.isEnabled = false
+            indic = indicatorButton.isEnabled
+            indicatorButton.text = "Unable"
+        }
     }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putInt("TAP_AMOUNTS", counter.currentCount)
+    }
+
 }
--- app/src/main/java/com/example/secondhomework/bonch/dev/school/SecondActivity.kt
+++ app/src/main/java/com/example/secondhomework/bonch/dev/school/SecondActivity.kt
@@ -13,5 +13,16 @@ class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+
+        textView = findViewById(R.id.text_view)
+
+        val counterValue = intent.getIntExtra("TAP_AMOUNTS", 0)
+        val indicatorValue = intent.getBooleanExtra("INDICATOR", true)
+        val textValue = intent.getStringExtra("INPUT_TEXT")
+
+        textView.setText(
+                "Is Indicator button pressed? ${!indicatorValue}\n" +
+                "Button tapped ${counterValue} times\n" +
+                "Input text is ${textValue}")
     }
 }
